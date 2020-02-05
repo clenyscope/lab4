61c414ac6c5c5969554c9554aebd00618b4b7445
Cleny Clendenin <cleny.clendenin@pcc.edu>
2020-02-04 17:58:18 -0800

    removed println test comment. added conversion functions test java file.

--- src/edu/pcc/cis/conversion/ConversionFunctions.java
+++ src/edu/pcc/cis/conversion/ConversionFunctions.java
@@ -12,9 +12,6 @@ public class ConversionFunctions {
     private static final String[] MONTH_NAMES = { "", "January", "February", "March", "April",
             "May", "June", "July", "August", "September", "October", "November", "December" };
 
-    public static void main(String[] args){
-        System.out.println("this is a test");
-    }
     /**
      * Convert a temperature from celsius to fahrenheit
      * @param celsius temperature in degrees celsius
